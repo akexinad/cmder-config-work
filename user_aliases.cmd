e53f14c179ab9be834c86deeb4f94261cd4f8698
Ceccattini <Danny.Ceccattini@lendlease.com>
2019-10-30 10:37:20 +1100

    fixed

--- user_aliases.cmd
+++ user_aliases.cmd
@@ -36,8 +36,9 @@ gplom=git pull origin master
 gagcm=git add . && git commit -m $1
 gb=git branch
 gbd=git branch -D $1
-gcb=git checkout $1
+gchk=git checkout $1
 gcbf=git checkout -B feature/$1
+gcf=git checkout feature/$1
 gbdf=git branch -D feature/$1
 
 nrs=npm run start
